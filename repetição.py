f3f03501a309683d40642ffe69d8e19b4048f116
AramisLins <122841235+AramisLins@users.noreply.github.com>
2023-04-17 08:22:17 -0300

    Add files via upload

---
+++ repetição.py
@@ -0,0 +1,13 @@
+a = 1
+while (a > 0):
+  a = int(input("Digite um numero: "))
+  if a > 0:
+    print("o dobro é ", a * 2)
+
+print("Saiu da repetição")
+
+a = 1
+while (a <= 10):
+  print("n:", a)
+  a = a + 1
+  
